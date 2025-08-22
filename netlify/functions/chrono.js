16099035270246a694f74826a5e14452c22c5881
Seth Lee <sethlimlee@gmail.com>
2025-08-21 21:49:03 -0600

    asdasdasdas

--- netlify/functions/chrono.js
+++ netlify/functions/chrono.js
@@ -4,13 +4,9 @@ exports.handler = async (event, context) => {
   const { queryStringParameters } = event;
   const { url } = queryStringParameters;
   
-
-  console.log('API Token Value:', process.env.APIFY_API_TOKEN);
-  // Replace with your actual Apify API token and Task ID
   const APIFY_API_TOKEN = process.env.APIFY_API_TOKEN;
   const CHRONOGOLF_TASK_ID = 'NiJvcWHVOkWBWNser'; 
 
-  // Initialize the ApifyClient with your API token
   const apifyClient = new ApifyClient({
     token: APIFY_API_TOKEN,
   });
@@ -23,19 +19,16 @@ exports.handler = async (event, context) => {
   }
 
   try {
-    // Run the Apify task with a dynamic start URL
     const run = await apifyClient.task(CHRONOGOLF_TASK_ID).call({
-      // The startUrls is now a dynamic value based on the URL parameter
       startUrls: [{ url: url }],
       useSession: true,
-      waitUntil: 'networkidle0',
+      // CORRECTED LINE: waitUntil now in an array
+      waitUntil: ['networkidle0'],
     });
 
-    // Get the dataset from the run and list the items
     const dataset = await apifyClient.dataset(run.defaultDatasetId).listItems();
 
     if (dataset.items && dataset.items.length > 0) {
-      // Return the first item from the dataset, assuming it's the JSON data
       return {
         statusCode: 200,
         body: JSON.stringify(dataset.items[0]),
