cf1e463231f5bc5d9292361e7c797b25fed65888
Seth Lee <sethlimlee@gmail.com>
2025-08-21 21:56:27 -0600

    dfgdfgdfg

--- src/App.js
+++ src/App.js
@@ -275,7 +275,7 @@ function App() {
   return (
     <div>
       <div className="header">
-        <h1 className="title-green">MPLLC.GOLF</h1>
+        <h1 className="title-green">Site is Currently Down. Chronogolf blocked us... Site is Currently Down. Chronogolf blocked us... Site is Currently Down. Chronogolf blocked us...</h1>
         Select a Date to View Tee Times
   
         {/* Wrap DatePicker with a container for margin */}
